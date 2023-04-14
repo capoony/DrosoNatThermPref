b4c221b06d7ba49b4b18f25c7d198a76d79d1896
Martin Kapun <capoony@gmail.com>
2023-04-14 10:40:54 +0200

    update

--- analyses/MK/Analyses.r
+++ analyses/MK/Analyses.r
@@ -10,7 +10,9 @@ setwd("D:/GitHub/DrosoNatThermPref/analyses")
 DATA <- read_excel("../data/Tp_data_transformed_MK.xlsx")
 DATA$Run <- factor(DATA$Run)
 DATA$slab_lane <- factor(DATA$slab_lane)
+DATA$replica <- factor(DATA$replica)
 DATA$ID2 <- paste(DATA$Wolbachia_status, DATA$country, DATA$BioRep, sep = "_")
+DATA$RepID <- paste0(DATA$Wolbachia_strain, DATA$replica)
 
 ## reorder Strains
 DATA$Wolbachia_strain <- factor(DATA$Wolbachia_strain, levels = c("Uninf_Po_1_-", "Uninf-AB_Po_1_-", "Uninf_Po_2_-", "Uninf-AB_Po_2_-", "wMel_Po_1_+", "wMel_Po_1_-", "wMel_Po_2_+", "wMel_Po_2_-", "wMel_Fi_1_+", "wMel_Fi_1_-", "wMel_Fi_2_+", "wMel_Fi_2_-", "wMelCS_Po_1_+", "wMelCS_Po_1_-", "wMelCS_Po_2_+", "wMelCS_Po_2_-"))
@@ -22,9 +24,11 @@ DATA2 <- DATA
 DATA2$Wolbachia_status[DATA2$Wolbachia_status == "Uninf-AB"] <- "Uninf"
 DATA2$ID2 <- paste(DATA2$Wolbachia_status, DATA2$country, DATA2$BioRep, sep = "_")
 
+
 means <- DATA2 %>%
     group_by(ID2) %>%
     dplyr::summarise(Mean = mean(TempEst), SD = sd(TempEst), SE = SD / sqrt(n()), Median = median(TempEst))
+
 
 write.table(
     file = "MK/TP_means.txt",
@@ -32,7 +36,6 @@ write.table(
     quote = F,
     row.names = F
 )
-
 
 
 ## now plot averages
@@ -63,8 +66,8 @@ ggsave(
 )
 
 ## now plot by replicates
-TP.replicas <- ggplot(DATA, aes(x = Wolbachia_strain, y = TempEst, col = Wolbachia_strain, fill = Wolbachia_strain)) +
-    facet_grid(. ~ replica, scales = "free_x", space = "free") +
+TP.replicas <- ggplot(DATA, aes(x = as.factor(replica), y = TempEst, col = Wolbachia_strain, fill = Wolbachia_strain)) +
+    facet_grid(. ~ Wolbachia_strain, scales = "free_x", space = "free") +
     geom_jitter(alpha = 0.1, show.legend = FALSE) +
     geom_boxplot(alpha = 0.5, show.legend = FALSE) +
     theme_bw() +
@@ -97,6 +100,7 @@ sink("MK/Stats.txt")
 options(contrasts = c("contr.sum", "contr.poly"))
 
 #### First test, is there a difference between lines and AB treatment
+
 RES.line <- lmer(TempEst ~ line * antibiotic_treatment + ambient_temp + humidity + (1 | Replicate) + (1 | slab_lane / Run),
     data = DATA
 )
@@ -109,9 +113,6 @@ print(kable(Anova(RES.line,
 cat("\n#### PostHoc Test ####")
 print(kable(pairs(emmeans(RES.line, ~ antibiotic_treatment | line))))
 
-DATA2 <- DATA
-DATA2$Wolbachia_status[DATA2$Wolbachia_status == "Uninf-AB"] <- "Uninf"
-
 #### Then test, if there is a difference between Wolbachia status and AB treatment
 RES.Wolb <- lmer(TempEst ~ Wolbachia_status * antibiotic_treatment + country * antibiotic_treatment + ambient_temp + humidity + (1 | line) + (1 | Replicate) + (1 | slab_lane / Run) + (1 | BioRep / country),
     data = DATA2
@@ -123,10 +124,19 @@ print(kable(Anova(RES.Wolb,
     type = 3
 )))
 
+RES.Rep <- lmer(TempEst ~ Replicate + ambient_temp + humidity + (1 | line) + (1 | slab_lane / Run) + (1 | BioRep / country),
+    data = DATA2
+)
+
+cat("\n#### Test by Wolbachia Status ####")
+
+print(kable(Anova(RES.Rep)))
+
 cat("\n#### PostHoc Test ####")
 print(kable(pairs(emmeans(RES.Wolb, ~ antibiotic_treatment | Wolbachia_status))))
 
 sink()
+
 
 means <- DATA2 %>%
     group_by(Wolbachia_status, antibiotic_treatment) %>%
@@ -154,5 +164,107 @@ ggsave(
     "MK/TP_interaction.png",
     TP,
     width = 8,
+    height = 6
+)
+
+
+## what about replicates?? Does it matter to remove clustered samples?
+
+means2 <- DATA2 %>%
+    group_by(RepID) %>%
+    dplyr::summarise(
+        Mean = mean(TempEst),
+        SD = sd(TempEst),
+        SE = SD / sqrt(n()),
+        Median = median(TempEst),
+        N = n(),
+        N90 = sum(TempEst < min(TempEst) + 2)
+    ) %>%
+    ## identify samples with more than 80% within 2°C from minimum where the median is < 20°C
+    filter(Median < 20 & N90 / N > 0.80)
+
+## remove those samples
+DATA3 <- DATA2 %>%
+    filter(!RepID %in% means2$RepID)
+
+RES.line <- lmer(TempEst ~ line * antibiotic_treatment + ambient_temp + humidity + (1 | Replicate) + (1 | slab_lane / Run),
+    data = DATA3
+)
+
+sink("MK/Stats_reduced.txt")
+cat("#### Test by Lines ####")
+print(kable(Anova(RES.line,
+    type = 3
+)))
+
+cat("\n#### PostHoc Test ####")
+print(kable(pairs(emmeans(RES.line, ~ antibiotic_treatment | line))))
+
+cat("\n#### Test by Wolbachia Status ####")
+
+#### Then test, if there is a difference between Wolbachia status and AB treatment
+RES.Wolb <- lmer(TempEst ~ Wolbachia_status * antibiotic_treatment + country * antibiotic_treatment + ambient_temp + humidity + (1 | line) + (1 | Replicate) + (1 | slab_lane / Run) + (1 | BioRep / country),
+    data = DATA3
+)
+
+cat("\n#### Test by Wolbachia Status ####")
+
+print(kable(Anova(RES.Wolb,
+    type = 3
+)))
+cat("\n#### PostHoc Test ####")
+print(kable(pairs(emmeans(RES.Wolb, ~ antibiotic_treatment | Wolbachia_status))))
+
+sink()
+
+TP.plot <- ggplot(DATA3, aes(x = Wolbachia_strain, y = TempEst, col = Wolbachia_strain, fill = Wolbachia_strain)) +
+    facet_grid(. ~ country, scales = "free_x", space = "free") +
+    geom_jitter(alpha = 0.1, show.legend = FALSE) +
+    geom_boxplot(alpha = 0.5, show.legend = FALSE) +
+    theme_bw() +
+    theme(text = element_text(size = 10)) +
+    xlab("SampleID (Wolbachiastrain,Origin,Infectionstatus)") +
+    scale_y_continuous(name = "Temperature (°C)", breaks = seq(10, 36, 1)) +
+    scale_color_manual(values = color) +
+    scale_fill_manual(values = color) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+ggsave(
+    "MK/TP_plot_reduced.pdf",
+    TP.plot,
+    width = 8,
+    height = 6
+)
+
+ggsave(
+    "MK/TP_plot_reduced.png",
+    TP.plot,
+    width = 8,
+    height = 6
+)
+
+TP.replicas <- ggplot(DATA3, aes(x = as.factor(replica), y = TempEst, col = Wolbachia_strain, fill = Wolbachia_strain)) +
+    facet_grid(. ~ Wolbachia_strain, scales = "free_x", space = "free") +
+    geom_jitter(alpha = 0.1, show.legend = FALSE) +
+    geom_boxplot(alpha = 0.5, show.legend = FALSE) +
+    theme_bw() +
+    theme(text = element_text(size = 10)) +
+    xlab("SampleID (Wolbachiastrain,Origin,Infectionstatus)") +
+    scale_y_continuous(name = "Temperature (°C)", breaks = seq(10, 36, 1)) +
+    scale_color_manual(values = color) +
+    scale_fill_manual(values = color) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+ggsave(
+    "MK/TP_replicas_reduced.pdf",
+    TP.replicas,
+    width = 20,
+    height = 6
+)
+
+ggsave(
+    "MK/TP_replicas_reduced.png",
+    TP.replicas,
+    width = 20,
     height = 6
 )
